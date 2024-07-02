df9b2423b3d4420bac160c38c5727e0e308e29c2
lynnOUYANG <144458512+lynnOUYANG@users.noreply.github.com>
2024-07-02 17:25:20 +0800

    Add files via upload

---
+++ src/run.py
@@ -0,0 +1,101 @@
+import tensorflow as tf
+from tensorflow.python.util import deprecation
+deprecation._PRINT_DEPRECATION_WARNINGS = False
+# from sklearn.kernel_approximation import RBFSampler, PolynomialCountSketch, Nystroem
+# from sklearn.feature_extraction.text import TfidfTransformer
+# from sklearn.neighbors import kneighbors_graph
+from time import time
+from scipy import sparse
+from sklearn.cluster import KMeans
+from utils import *
+from sklearn.utils.extmath import randomized_svd
+from sklearn import cluster
+
+
+flags = tf.compat.v1.flags
+FLAGS = flags.FLAGS
+
+
+# Parameters
+flags.DEFINE_string('dataset', 'acm', 'Name of the graph dataset (`acm`, `dblp`, `arxiv`, `pubmed` or `wiki`).')
+flags.DEFINE_integer('tau', 10, 'Propagation order.')
+flags.DEFINE_integer('runs', 5, 'Number of runs per power.')
+flags.DEFINE_float('alpha', '0.9', 'decay.')
+flags.DEFINE_integer('fdim', '0', 'feature dimension.')
+flags.DEFINE_string('method', 'sub', 'the method in to use.')
+flags.DEFINE_float('gamma', '0.9', 'the method in to use.')
+flags.DEFINE_integer('T', 7, 'the itertate rounds')
+flags.DEFINE_bool('label_pre', False, 'the itertate rounds')
+
+dataset = flags.FLAGS.dataset
+p = flags.FLAGS.tau
+n_runs = flags.FLAGS.runs
+alpha= flags.FLAGS.alpha
+fdim = flags.FLAGS.fdim
+method = flags.FLAGS.method
+gamma=flags.FLAGS.gamma
+T=flags.FLAGS.T
+label_pre = flags.FLAGS.label_pre
+adj, features, labels, n_classes =  datagen(dataset)
+
+if fdim>0 and fdim>n_classes:
+    print("reduce attribute dim")
+    U, Sigma, _ = randomized_svd(features, n_components=fdim, random_state=42)
+    features = sparse.csr_matrix(U).dot(sparse.diags(Sigma))
+
+norm_adj, features = preprocess_dataset(adj, features,
+                                      tf_idf=True,
+                                      sparse=True)
+
+
+features = features.toarray()
+n, d = features.shape
+k = n_classes
+
+
+metrics = {}
+metrics['acc'] = []
+metrics['nmi'] = []
+metrics['ari'] = []
+metrics['time'] = []
+adj_pt=sparse.coo_matrix(adj)
+
+
+x = features
+
+
+for run in range(n_runs):
+    features = x
+
+    t0 = time()
+
+    P, Q = run_my_model(features, k, norm_adj, p, alpha,method=method,dataset=dataset,gamma=gamma,T=T)
+
+
+
+metrics['time'].append(time()-t0)
+metrics['acc'].append(clustering_accuracy(labels, P)*100)
+# from sklearn.metrics import accuracy_score
+# metrics['acc'].append(accuracy_score(labels, P) * 100)
+metrics['nmi'].append(nmi(labels, P)*100)
+metrics['ari'].append(ari(labels, P)*100)
+
+
+results = {
+      'mean': {k:(np.mean(v)).round(2) for k,v in metrics.items() }, 
+      'std': {k:(np.std(v)).round(2) for k,v in metrics.items()}
+    }
+
+means = results['mean']
+std = results['std']
+
+
+print(f"{dataset} {p} {alpha} {method} {T}")
+print(f"acc: {means['acc']}±{std['acc']} & nmi: {means['nmi']}±{std['nmi']} & ari: {means['ari']}±{std['ari']} & time: {means['time']}±{std['time']}", sep=',')
+def append_text_to_file(text, filename):
+    with open(filename,'a') as file:
+        file.write(text+'\n')
+file_name=f"{dataset}_batch.txt"
+append_text_to_file(f"tau: {p} & alpha: {alpha} & method: {method} & T: {T} & acc: {means['acc']}±{std['acc']} & nmi: {means['nmi']}±{std['nmi']} & ari: {means['ari']}±{std['ari']} & time: {means['time']}±{std['time']}",file_name)
+
+# print(f"{means['time']}±{std['time']} seconds")
---
+++ src/utils.py
@@ -0,0 +1,170 @@
+from sklearn.utils.extmath import randomized_svd
+from sklearn.preprocessing import normalize
+import numpy as np
+import scipy.sparse as sp
+from sklearn.preprocessing import normalize
+from sklearn.metrics import normalized_mutual_info_score as nmi
+from sklearn.metrics import adjusted_rand_score as ari, davies_bouldin_score
+from sklearn.feature_extraction.text import TfidfTransformer
+from time import time
+from scipy import sparse
+import warnings
+import tensorflow as tf
+warnings.filterwarnings('ignore')
+
+# from ogb.nodeproppred import NodePropPredDataset
+import numpy as np
+from scipy.sparse import csr_matrix
+import scipy.io as io
+import os
+
+from sklearn.utils.extmath import randomized_svd
+from sklearn import preprocessing
+
+from func import  SNEM_rounding, subspace_svd,KSI_decompose_B,sub_randomized_svd
+
+
+def datagen(dataset):
+  if dataset in ['wiki', 'pubmed', 'computers', 'acm', 'dblp', 'citeseer', 'corafull','arxiv']:
+    data = io.loadmat(os.path.join('data', f'{dataset}.mat'))
+    features = data['fea'].astype(float)
+    adj = data.get('W')
+
+
+    if adj is not None:
+      adj = adj.astype(float)
+      if not sp.issparse(adj):
+          adj = sp.csc_matrix(adj)
+
+
+    if not sparse and sp.issparse(features):
+        features = features.toarray()
+    if dataset in ['wiki', 'pubmed', 'computers', 'citeseer', 'corafull','arxiv']:
+        labels = data['gnd'].reshape(-1) - 1
+    else:
+        labels=data['gnd'].reshape(-1)
+    n_classes = len(np.unique(labels))
+
+    return adj, features, labels, n_classes
+
+    return adj, features, labels, n_classes
+  if dataset in ['Amazon_photos']:
+      data = io.loadmat(os.path.join('data', f'{dataset}.mat'))
+      features = data['features'].astype(float)
+      adj = data.get('adj')
+      if adj is not None:
+          adj = adj.astype(float)
+          if not sp.issparse(adj):
+              adj = sp.csc_matrix(adj)
+
+      if not sparse and sp.issparse(features):
+          features = features.toarray()
+      labels = data['label'].reshape(-1)
+      n_classes = len(np.unique(labels))
+      return adj, features, labels, n_classes
+     
+
+def preprocess_dataset(adj, features, row_norm=True, sym_norm=True, feat_norm='l2', tf_idf=False, sparse=False, alpha=1, beta=1):
+
+    if sym_norm:
+        adj = aug_normalized_adjacency(adj, True, alpha=alpha)
+    if row_norm:
+        adj = row_normalize(adj, True, alpha=beta)
+        # adj=similarity_M(adj,True)
+
+
+    if tf_idf:
+        features = TfidfTransformer(norm=feat_norm).fit_transform(features)
+    else:
+        features = normalize(features, feat_norm)
+    
+    if not sparse:
+        features = features.toarray()
+    return adj, features
+
+def aug_normalized_adjacency(adj, add_loops=True, alpha=1):
+    if add_loops:
+        adj = adj + alpha*sp.eye(adj.shape[0])
+    adj = sp.coo_matrix(adj)
+    row_sum = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(row_sum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+
+    return d_mat_inv_sqrt.dot(adj).dot(d_mat_inv_sqrt).tocoo()
+
+
+def row_normalize(mx, add_loops=True, alpha=1):
+    if add_loops:
+        mx = mx + alpha * sp.eye(mx.shape[0])
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+
+def convert_sparse_matrix_to_sparse_tensor(X):
+      coo = X.tocoo()
+      indices = np.mat([coo.row, coo.col]).transpose()
+      return tf.SparseTensor(indices, coo.data, coo.shape)
+
+
+def clustering_accuracy(y_true, y_pred):
+    from sklearn.metrics import confusion_matrix
+    from scipy.optimize import linear_sum_assignment
+
+    def ordered_confusion_matrix(y_true, y_pred):
+      conf_mat = confusion_matrix(y_true, y_pred)
+      w = np.max(conf_mat) - conf_mat
+      row_ind, col_ind = linear_sum_assignment(w)
+      conf_mat = conf_mat[row_ind, :]
+      conf_mat = conf_mat[:, col_ind]
+      return conf_mat
+
+    conf_mat = ordered_confusion_matrix(y_true, y_pred)
+    return np.trace(conf_mat) / np.sum(conf_mat)
+
+
+# @tf.function
+def my_convolve(feature, adj_normalized, power, alpha):
+  #feature = preprocessing.normalize(feature, norm='l2', axis=1)
+  feat0 = feature.copy()
+  factor = power if alpha == 1.0 else (1.-alpha)/ (1. - np.power(alpha, power + 1))
+  for _ in range(power):
+    feature = alpha*(adj_normalized.dot(feature)+feat0)
+
+  feature=factor*feature
+
+  return feature
+
+
+
+def run_my_model(X, k, adj_normalized, power, alpha,method="sub",dataset='acm',beta=0.9,T=7):
+
+  x = X.sum(0)
+  D = X @ x
+  D[D==0]=1
+
+  D = np.sqrt(D)
+  D = sparse.diags(1.0/D)
+  X = D.dot(X)
+  m = X.shape[0]
+  n = X.shape[1]
+  nnz=adj_normalized.nnz
+  if method =='sub':
+     cost1=2*m*n+power*(nnz*n+m*n)+2*7*(m*n*(k+11))+2*m*n*(k+11)+(2*m+n)*(k+11)**2
+     cost2=2*7*(m*n*(k+10)+2*m*(k+10)+power*(nnz*(k+10)+m*(k+10)))+2*m*n*(k+11)+m*(k+10)*2+power*(nnz*(k+10)+m*(k+10))+(2*m+n)*(k+11)**2
+     if cost1<cost2:
+         Z = my_convolve(X, adj_normalized, power, alpha)
+         Q= sub_randomized_svd(Z, n_components=k,n_iter=T)
+
+     else:
+        Q = subspace_svd(adj_normalized, X, n_components=k, n_iter=T,n_T=power, alpha=alpha)
+
+  elif method == 'mod':
+          Z = my_convolve(X, adj_normalized, power, alpha)
+          Q = KSI_decompose_B(Z=Z, dim=k,T=T,beta=beta)
+  P = SNEM_rounding(Q)
+  return P, Q
