cfd4c251aa378a02be9ca44aa40db822bb62a400
lynnOUYANG <144458512+lynnOUYANG@users.noreply.github.com>
2024-07-31 13:45:13 +0800

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,28 +1,28 @@
-python run.py --dataset=acm --tau 15 --alpha 0.8 --method mod  --gamma 1 --T 50
-python run.py --dataset=acm --tau 15 --alpha 0.8 --method sub --gamma 1 --T 7
-#
-python run.py --dataset=dblp --tau 10 --alpha 0.9 --method mod --gamma 1 --T 50
-python run.py --dataset=dblp --tau 10 --alpha 0.9 --method sub --gamma 1 --T 7
+python run.py --dataset=acm --T 15 --alpha 0.8 --method mod  --gamma 1 --tau 50
+python run.py --dataset=acm --T 15 --alpha 0.8 --method sub --gamma 1 --tau 7
+
+python run.py --dataset=dblp --T 10 --alpha 0.9 --method mod --gamma 1 --tau 50
+python run.py --dataset=dblp --T 10 --alpha 0.9 --method sub --gamma 1 --tau 7
 
 
-python run.py --dataset=wiki --tau 12 --alpha 1.7 --method mod --gamma 0.9 --T 50
-python run.py --dataset=wiki --tau 6 --alpha 1.7 --method sub --gamma 0.9 --T 7
+python run.py --dataset=wiki --T 12 --alpha 1.7 --method mod --gamma 0.9 --tau 50
+python run.py --dataset=wiki --T 6 --alpha 1.7 --method sub --gamma 0.9 --tau 7
 
 
-python run.py --dataset=pubmed --tau 175 --alpha 1.8 --method mod --gamma 1 --T 50
-python run.py --dataset=pubmed --tau 175 --alpha 1.8 --method sub --gamma 1 --T 7
+python run.py --dataset=pubmed --T 175 --alpha 1.8 --method mod --gamma 1 --tau 50
+python run.py --dataset=pubmed --T 175 --alpha 1.8 --method sub --gamma 1 --tau 7
 
 
-python run.py --dataset=arxiv --tau 30 --alpha 2.5 --method mod --gamma 1 --T 50
-python run.py --dataset=arxiv --tau 30 --alpha 1.4 --method sub --gamma 1 --T 4
+python run.py --dataset=arxiv --T 30 --alpha 2.5 --method mod --gamma 1 --tau 50
+python run.py --dataset=arxiv --T 30 --alpha 1.4 --method sub --gamma 1 --tau 4
 
 
-python run.py --dataset=corafull --tau 7 --alpha 1.4 --method mod --gamma 1 --T 100
-python run.py --dataset=corafull --tau 20 --alpha 0.9 --method sub --gamma 1 --T 7
+python run.py --dataset=corafull --T 7 --alpha 1.4 --method mod --gamma 1 --tau 100
+python run.py --dataset=corafull --T 20 --alpha 0.9 --method sub --gamma 1 --tau 7
 
 
-python run.py --dataset=citeseer --tau 40 --alpha 0.8 --method mod --gamma 0.9 --T 100
-python run.py --dataset=citeseer --tau 60 --alpha 0.8 --method sub --gamma 0.9 --T 7
+python run.py --dataset=citeseer --T 40 --alpha 0.8 --method mod --gamma 0.9 --tau 100
+python run.py --dataset=citeseer --T 60 --alpha 0.8 --method sub --gamma 0.9 --tau 7
 
-python run.py --dataset=Amazon_photos --tau 9 --alpha 1.5 --method mod --gamma 1 --T 50
-python run.py --dataset=Amazon_photos --tau 9 --alpha 1.5 --method sub --gamma 1 --T 7
+python run.py --dataset=Amazon_photos --T 9 --alpha 1.5 --method mod --gamma 1 --tau 50
+python run.py --dataset=Amazon_photos --T 9 --alpha 1.5 --method sub --gamma 1 --tau 7
